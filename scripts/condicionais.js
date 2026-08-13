3fc43fc51d60984b9ac37c0fc3c20731f9452f5e
André Gomes <andregjm95@gmail.com>
2026-08-13 09:24:10 -0300

    conditionals

---
+++ scripts/condicionais.js
@@ -0,0 +1,50 @@
+let sum = 1 + 3;
+
+
+// if (sum === 2) { //=== checa o valor e o tipo
+//     console.log(`sum is ${sum}`);
+// } else {
+//     console.log("sum is not 2" )
+// };
+
+//&& = and, || = or
+
+// const idade = 21;
+
+// if (idade === 20 && idade > 22) {
+//     console.log(idade)
+// }
+// else{
+//     console.log('errado')
+// };
+
+// let numero = sum === 2 ? 2 : 4; //condição ? valorSeVerdadeiro : valorSeFalso;
+
+// // if (idade === 21) {
+// //     numero = 10
+// // }
+// // else {
+// //     numero = 22
+// // };
+
+// console.log(numero);
+
+const Jogos = 'Gta ';
+
+switch (Jogos) {
+    case 'The legends of zelda' :
+        console.log('jogasso da p****!')
+        break;
+
+    case 'fran bow' :
+        console.log('minha infancia todinha')
+        break;
+
+    case 'sally face' :
+        console.log('um amor por esse jogo')
+        break;
+    
+    default :
+    console.log('Jogo invalido')
+    break;
+}
