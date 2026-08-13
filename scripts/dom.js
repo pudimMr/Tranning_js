ad389e2b24f527066c0ae7e261dda080cfbc3536
André Gomes <andregjm95@gmail.com>
2026-08-13 15:45:51 -0300

    elements, DOM, HTML & CSS

---
+++ scripts/dom.js
@@ -0,0 +1,8 @@
+//SELECIONA UM ELEMENTO
+// const nomeTExt = document.querySelector('#paragrafo');
+// nomeTExt.textContent = 'PARTE'
+// console.log(nomeTExt);
+
+// const ALLlabels = document.querySelectorAll('label');
+// console.log(ALLlabels)
+
